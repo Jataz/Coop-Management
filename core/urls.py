cd729de7d94ea641080bf4f6a20277db21f15c25
jataz <africajataz@gmail.com>
2024-06-15 21:43:04 +0200

    First Commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+"""core URL Configuration
+
+
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('coop.urls')),
+]
